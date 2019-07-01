9e9579e51ad3c22ba2a76b3086950eb978261d87
thaiguitar <thaiguitar@windowslive.com>
2019-07-01 15:53:13 +0700

    finish

--- exercise7.js
+++ exercise7.js
@@ -1,8 +1,13 @@
 let number = "123.34";
 let numToword = {
     "1": "one",
-    "2": "two"
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
 }
 for (var i=0;i<number.length;i++){
-    console.log(number[i]);
+    //console.log(number[i]);
+    console.log(numToword[number[i]]);
 }
