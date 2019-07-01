bd5159232320b96a1051055b8010e2147eb773fe
thaiguitar <thaiguitar@windowslive.com>
2019-07-01 15:14:28 +0700

    15.14

---
+++ exercise7.js
@@ -0,0 +1,8 @@
+let number = "123.34";
+let numToword = {
+    "1": "one",
+    "2": "two"
+}
+for (var i=0;i<number.length;i++){
+    console.log(number[i]);
+}
---
+++ exericise1.js
@@ -0,0 +1,13 @@
+//ค่า ตัวเลข
+const number1 = 5;
+const number2 =10.0;
+console.log(number2 - number1);
+console.log(typeof(number1));
+// ผลลัพท์คือ 5 number
+
+//ค่าตัวแปร
+console.log(null == undefined);
+console.log(!!true);
+console.log(!!!true);
+console.log(!!!1);
+console.log(!!undefined);
